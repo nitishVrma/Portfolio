757c3cea287b9adbf572e0a7c8066edea3044f6f
Nitish <ozeus459@gmail.com>
2021-09-17 01:01:55 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-
-import './App.css';
+import Topbar from "./components/topbar/Topbar";
+  import Intro from "./components/intro/Intro";
+  import Portfolio from "./components/portfolio/Portfolio";
+  import Works from "./components/works/Works";
+  import Contact from "./components/contact/Contact";
+  import "./app.scss"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Nitish Kumar Verma</h1>
+    <div className="app">
+      <Topbar></Topbar>
+      <div className="sections">
+        saada
+      <Intro></Intro>
+      <Portfolio></Portfolio>
+      <Works></Works>
+      <Contact></Contact>
+      </div>
     </div>
   );
 }
