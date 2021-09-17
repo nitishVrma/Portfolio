13dea7342a5121fa7028fe0677500f277ed6f8e5
Nitish <ozeus459@gmail.com>
2021-09-17 13:21:11 +0530

    intro home page

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import Topbar from "./components/topbar/Topbar";
   import Works from "./components/works/Works";
   import Contact from "./components/contact/Contact";
   import "./app.scss"
+  import { useState } from "react";
+  import Menu from "./components/menu/Menu";
 
 function App() {
+
+  const [menuOpen,setMenuOpen] = useState(false)
   return (
     <div className="app">
-      <Topbar></Topbar>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}></Topbar>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}></Menu>
       <div className="sections">
-        saada
       <Intro></Intro>
       <Portfolio></Portfolio>
       <Works></Works>
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,23 @@ import "./intro.scss"
 
 export default function intro() {
     return (
-        <div className="intro">
+        <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/nkv.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>
+                        Hi There, I'm
+                    </h2>
+                    <h1>Nitish verma</h1>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
             
         </div>
     )
