74efcf49dbb4096574a839c84cc2950309a54854
Nitish <ozeus459@gmail.com>
2021-09-17 18:09:19 +0530

    contact section

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./topbar.scss"
-import {Person,Mail} from "@material-ui/icons"
+import {LinkedIn,Mail,GitHub} from "@material-ui/icons"
 
 export default function Topbar({menuOpen, setMenuOpen}) {
     return (
@@ -9,15 +9,27 @@ export default function Topbar({menuOpen, setMenuOpen}) {
                 <div className="left">
                     <a href="#intro" className="logo">NKV</a>
                     <div className="itemContainer">
-                        <Person className="icon"></Person>
+                    
+                        {/* <Person className="icon"></Person> */}
                         {/* <span>+342 2234 234</span> */}
                     </div>
+                    
                     <div className="itemContainer">
                         <Mail className="icon"></Mail>
                         <span>nitish.kvrma@gmail.com</span>
                     </div>
+                    <div className="itemContainer">
+                    <LinkedIn/>
+                    <span>linkdin</span>
+                    </div>
+                    <div className="itemContainer">
+                    <GitHub/>
+                    <span>Github</span>
+                    
+                    </div>
                 </div>
                 <div className="right">
+                    
                     <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
                         <span className="line1"></span>
                         <span className="line2"></span>
